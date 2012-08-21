94b90aa928cf6d53fe2630ca7a8ec269a07c8a17
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-21 10:37:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlairExample::Application.config.secret_token = 'a6be8fe9bd0b30ed99023347eb71f5ea139e14e67cea0a3191c7dd22c976f10419d6b75794d8b70a6cf44dee846fb1ab1371249e53ae8ee96d2cbe92563f1a22'
